1ae32a7123b1ebeca04f3fbc7401d2b5ee48bb5f
Jiramate Kedmake <jiramate2548@gmail.com>
2024-12-26 18:09:30 +0700

    Make a grid change color when mouse hover over it

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ let gridContainer = document.querySelector('#grid-container');
 
 const gridSize = 16;
 
+function hover(target){
+    target.style.backgroundColor = 'grey';
+}
+
 for (let i=0; i<gridSize*gridSize; i++){
     let grid = document.createElement('div');
     grid.classList.toggle('grid-square');
+    grid.addEventListener('mouseenter', (e) => hover(e.target));
     gridContainer.appendChild(grid);
 }
